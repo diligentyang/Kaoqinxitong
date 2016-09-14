58f9916f60c1c8eae39f240fcc3ef84327742299
diligentyang <905407339@qq.com>
2016-09-14 14:14:56 +0800

    flat-ui

--- application/views/Backend/super_index.php
+++ application/views/Backend/super_index.php
@@ -1,3 +1,57 @@
-<?php 
-echo "this is super_index";
-?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <base href="<?php echo base_url();?>">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="BackendStyle/css/bootstrap.min.css" rel="stylesheet">
+	<link href="BackendStyle/css/flat-ui.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+	<div class="col-xs-3">
+          <h4 class="demo-panel-title">年级</h4>
+          <select class="form-control select select-primary" data-toggle="select">
+            <option value="0">Choose hero</option>
+            <option value="1">Spider Man</option>
+            <option value="2">Wolverine</option>
+            <option value="3">Captain America</option>
+            <option value="4" selected>X-Men</option>
+            <option value="5">Crocodile</option>
+          </select>
+    </div> <!-- /.col-xs-3 -->
+	
+	<div class="col-xs-3">
+          <h4 class="demo-panel-title">班级</h4>
+          <select class="form-control select select-primary" data-toggle="select">
+            <option value="0">Choose hero</option>
+            <option value="1">Spider Man</option>
+            <option value="2">Wolverine</option>
+            <option value="3">Captain America</option>
+            <option value="4" selected>X-Men</option>
+            <option value="5">Crocodile</option>
+          </select>
+    </div> <!-- /.col-xs-3 -->
+	
+	<div class="col-xs-3">
+          <h4 class="demo-panel-title">课程</h4>
+          <select class="form-control select select-primary" data-toggle="select">
+            <option value="0">Choose hero</option>
+            <option value="1">Spider Man</option>
+            <option value="2">Wolverine</option>
+            <option value="3">Captain America</option>
+            <option value="4" selected>X-Men</option>
+            <option value="5">Crocodile</option>
+          </select>
+    </div> <!-- /.col-xs-3 -->
+	
+</div>
+<script src="BackendStyle/js/jquery.min.js"></script>
+<script src="BackendStyle/js/bootstrap.min.js"></script>
+<script src="BackendStyle/js/flat-ui.min.js"></script>
+<script src="BackendStyle/js/application.js"></script>
+</body>
+<html>
