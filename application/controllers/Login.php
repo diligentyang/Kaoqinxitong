494f53bacda4e46829c9140ecb14903007f2b557
diligentyang <905407339@qq.com>
2016-09-14 09:11:07 +0800

    update login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -23,31 +23,40 @@ class Login extends CI_Controller
 		$super=$this->input->post("super");
 		if($super){
 			$superinfo = $this->Loginmodel->get_super_info();
+			if (empty($superinfo)) {
+				$data['error']="用户名密码不正确";
+				$this->load->view("Backend/login",$data);
+			}else {
+				foreach ($superinfo as $value) {
+					$tID=$value['tID'];
+					echo $tID;
+				}
+			}
 		}else{
 			$userinfo=$this->Loginmodel->get_user_info();
 			if (empty($userinfo)) {
 				$data['error']="用户名密码不正确";
 				$this->load->view("Backend/login",$data);
 			} else {
-            foreach ($userinfo as $value) {
-                $tID=$value['tID'];
-                $tIsAssistant=$value['tIsAssistant'];
-            }
-            $hasClass=1;
-            $res=$this->Loginmodel->check_class($tID);
-            //echo $res->num;
-            if($res->num==0){
-                $hasClass=0;
-            }
-            $this->session->set_userdata("tID",$tID);
-            $this->session->set_userdata("isAssistant",$tIsAssistant);
-            $this->session->set_userdata("hasClass",$hasClass);
-            $this->session->set_userdata("admin","ok");
-            //$this->session->set_userdata("hasStudents","0");
-            redirect("Backend/index");
-        }
+				foreach ($userinfo as $value) {
+					$tID=$value['tID'];
+					$tIsAssistant=$value['tIsAssistant'];
+				}
+				$hasClass=1;
+				$res=$this->Loginmodel->check_class($tID);
+				//echo $res->num;
+				if($res->num==0){
+					$hasClass=0;
+				}
+				$this->session->set_userdata("tID",$tID);
+				$this->session->set_userdata("isAssistant",$tIsAssistant);
+				$this->session->set_userdata("hasClass",$hasClass);
+				$this->session->set_userdata("admin","ok");
+				//$this->session->set_userdata("hasStudents","0");
+				redirect("Backend/index");
+			}
 		}
-    }
+	}
 
 
 
